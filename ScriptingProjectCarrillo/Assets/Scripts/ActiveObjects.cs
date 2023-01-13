d2040fe85391e449a6a01e6ca24168f410f4133e
MackenzyCarrillo <709666@student.dusd.net>
2023-01-13 14:16:05 -0800

    Finished Scripting Project
    Finished Activating Game Objects assignment

---
+++ ScriptingProjectCarrillo/Assets/Scripts/ActiveObjects.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+public class ActiveObjects : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
