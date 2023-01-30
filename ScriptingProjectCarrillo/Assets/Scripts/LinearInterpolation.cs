74c44746dcccce22ad095d7b41914674cff4cf5b
MackenzyCarrillo <709666@student.dusd.net>
2023-01-30 15:28:33 -0800

    Linear Interpolation
    I did the Linear Interpolation Scripting Project assignment

---
+++ ScriptingProjectCarrillo/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{ /*
+  
+    // In this case, result = 4
+    // Mathf.Lerp is a function that is uses 3 float parameters. Two numbers and one number used to find thee percentage between the other two numbers .In this case, the values are 3 and 5, and the percentage is 0.5 or 50 percent. The result of this code equals 4 because 4 is 50% or halfway between 3 and 5. 
+    float result = Mathf.Lerp (3f, 5f, 0.5f);
+
+    //Another example of Mathf.Lerp
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+    
+    //Here results is going to be = (4, 5, 6)
+    Vector3 result = Vector3.Lerp (from, to, 0.75f);
+    //the result is (4, 5, 6) because 4 is 75% of the way between 1 and 5, 5 is 75% of the  way there between 2 and 6 and 6 is 75* of the way between 3 and 7.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+      
+       light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    
+      //While both Mathf.Lerp lines here control light intensity, the first line is dependent on frames meaning that the intensity updates every frame.The line below however has Time.deltaTime which means that intensity updates every second instead of frames.
+    
+    
+    }
+*/}
