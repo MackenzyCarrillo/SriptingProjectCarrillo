796c3fb983a7c5c4e88f1dc2dae069f4f057ce5c
MackenzyCarrillo <709666@student.dusd.net>
2023-02-01 15:28:46 -0800

    r
    r

---
+++ ScriptingProjectCarrillo/Assets/Scripts/DestroyOther.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DestroyOther : MonoBehaviour
+{
+    public GameObject other;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.O))
+        {
+            Destroy(other);
+        }
+    }
+}
