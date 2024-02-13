2c90fade239b767b4c2812ba243f69a052735eda
MackenzyCarrillo <709666@student.dusd.net>
2024-02-13 12:43:17 -0800

    Finished Overriding assignment

---
+++ ScriptingProjectCarrillo/Assets/Scripts/Overriding/FruitSalad1.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSalad1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Apple1 myApple = new Apple1();
+
+        myApple.SayHello();
+
+
+        myApple.SayHello();
+        myApple.Chop();
+
+
+        Fruit1 myFruit = new Fruit1();
+        myFruit.SayHello();
+        myFruit.Chop(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
